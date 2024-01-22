3fcca1c6a50dbd36d2274a6707d94e12d8c7643d
Gabriel Quaresma <fulco02@gmail.com>
2024-01-22 00:48:34 -0300

    refactor: remove unused lines

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,12 +1,10 @@
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Controller from 'App/Controllers/Http/Controller'
 import UserService from 'App/Services/UserService'
-import IService from 'types/interface/service'
 
 class UsersController extends Controller {
-  constructor(service: IService) {
-    super(service)
-  }
+  // constructor(service: IService) {
+  //   super(service)
+  // }
 }
 
 export default new UsersController(UserService)
