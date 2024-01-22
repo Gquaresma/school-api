941a55161acef88014cbf5bb390cc2145846c0a2
Gabriel Quaresma <fulco02@gmail.com>
2024-01-22 00:48:06 -0300

    refactor: try catch

--- app/Controllers/Http/Controller.ts
+++ app/Controllers/Http/Controller.ts
@@ -10,33 +10,53 @@ export default abstract class Controller implements IController {
   }
 
   public async index({ response }: HttpContextContract): Promise<any> {
-    const data = await this.service.all()
+    try {
+      const data = await this.service.all()
 
-    if (data.length === 0) return response.noContent()
+      if (data.length === 0) return response.noContent()
 
-    return response.ok(data)
+      return response.ok(data)
+    } catch (error) {
+      return response.badRequest({ message: error.message })
+    }
   }
 
   public async create({ request, response }: HttpContextContract): Promise<any> {
-    const data = await this.service.create(request.body())
-    return response.created(data)
+    try {
+      const data = await this.service.create(request.body())
+      return response.created(data)
+    } catch (error) {
+      return response.badRequest({ message: error.message })
+    }
   }
 
   public async show({ params, response }: HttpContextContract): Promise<any> {
-    const data = await this.service.find(params.id)
+    try {
+      const data = await this.service.find(params.id)
 
-    if (!data) return response.notFound({ message: 'Recurso não encontrado' })
+      if (!data) return response.notFound({ message: 'Recurso não encontrado' })
 
-    return response.ok(data)
+      return response.ok(data)
+    } catch (error) {
+      return response.badRequest({ message: error.message })
+    }
   }
 
   public async update({ params, request, response }: HttpContextContract): Promise<any> {
-    const data = await this.service.update(params.id, request.body())
-    return response.ok(data)
+    try {
+      const data = await this.service.update(params.id, request.body())
+      return response.ok(data)
+    } catch (error) {
+      return response.badRequest({ message: error.message })
+    }
   }
 
   public async destroy({ params, response }: HttpContextContract): Promise<any> {
-    await this.service.delete(params.id)
-    return response.noContent()
+    try {
+      await this.service.delete(params.id)
+      return response.noContent()
+    } catch (error) {
+      return response.badRequest({ message: error.message })
+    }
   }
 }
